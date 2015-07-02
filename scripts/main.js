ffa6385bad8f9dbdc7002d28b09a73bff0c999f8
Andrés González <andres@simbiotica.es>
2015-07-02 12:19:44 +0200

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,1 @@
+var RocketLauncher=function(){"use strict";new IntroductionView,new SignalsView,new DescriptionView,new FeaturesView};window.onload=function(){"use strict";new RocketLauncher},define("main",function(){});
