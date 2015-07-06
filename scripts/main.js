77c15babe829f63e8d286da8a924dad27271040c
Andrés González <andres@simbiotica.es>
2015-07-06 18:24:06 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -21654,7 +21654,13 @@ var SignalsView = Backbone.View.extend({
   initialize: function() {
     this.initVars();
     this.setListeners();
-    this.checkPlayVideo();
+
+    if (!this.checkIsIpad()) {
+      this.checkPlayVideo();
+    } else {
+      $('.video-container').addClass('unrevealed');
+      $('.gif-container').removeClass('unrevealed');
+    }
   },
 
   initVars: function(){
@@ -21677,6 +21683,10 @@ var SignalsView = Backbone.View.extend({
     $(window).on('scroll', _.bind(function() {
       this.checkPlayVideo();
     }, this));
+  },
+
+  checkIsIpad: function() {
+    return navigator.platform === 'iPad' ? true : false;
   },
 
   checkPlayVideo: function() {
