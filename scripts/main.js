5dfae5e5aa4f18b2158062da084471340195c62f
Andrés González <andres@simbiotica.es>
2015-07-07 13:25:09 +0200

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -21545,8 +21545,9 @@ var FeaturesView = Backbone.View.extend({
   el: '.features',
 
   events: {
-    'click .learn-more': 'setFeature',
-    'click .link-feature': 'setFeature'
+    'click a': 'setFeature'
+    // 'click .learn-more': 'setFeature',
+    // 'click .link-feature': 'setFeature'
   },
 
   setFeature: function(e) {
